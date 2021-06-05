762958b8eeeb830a40bedebce10fd7498fa4a5f8
tetunori <tetunori@gmail.com>
2021-06-05 15:08:52 +0900

    Update p5MovRec.js
    Support converting mp4.

--- p5MovRec.js
+++ p5MovRec.js
@@ -1,5 +1,4 @@
-
-// p5.movieRecorder
+// p5.MovRec
 
 // MIT License
 // Copyright (c) 2021 Tetsunori NAKAYAMA
@@ -7,65 +6,194 @@
 // A simple movie recording tool for p5.js canvas
 
 class P5MovRec {
-  constructor(opeKey = 82 /* 'r' */) {
-    this.movRecorder;
-    this.chunks = [];
-    this.opeKey = opeKey;
-    this.init();
+  movRecorder = undefined;
+  chunks = [];
+
+  static codecId = {
+    vp9: 'vp9',
+    h264: 'h264',
+  };
+  codec = undefined;
+
+  movTypeId = {
+    webm: 'webm',
+    mp4: 'mp4',
+  };
+  movType = this.movTypeId.webm;
+
+  /**
+   * Constructor.
+   *
+   * @param codec specify codec with P5MovRec.codecId.
+   *              We support vp9 and H264.
+   */
+  constructor(codec = P5MovRec.codecId.h264) {
+    this.codec = codec;
+    this.initMovRec();
   }
 
-  init() {
+  /**
+   * Initialize function.
+   */
+  initMovRec() {
+    // Get stream from canvas element.
     const stream = document.querySelector('canvas').captureStream();
+
+    // Prepare MediaRecorder
     const options = {
-      videoBitsPerSecond: 100000000,
-      mimeType: 'video/webm;codecs=vp9',
+      videoBitsPerSecond: 20000000, // 20,000 Kbps
+      mimeType: 'video/webm;codecs=' + this.codec,
     };
-    this.movRecorder = new MediaRecorder(stream, options);
+    const recorder = new MediaRecorder(stream, options);
 
-    this.movRecorder.ondataavailable = (e) => {
-      if (e.data.size) {
-        this.chunks.push(e.data);
-      }
-    };
-    this.movRecorder.onstop = this.generateMovie.bind(this);
+    recorder.ondataavailable = (e) => this.chunks.push(e.data);
+    recorder.onstop = this.generateMovie.bind(this);
 
-    console.log(String.fromCharCode(this.opeKey) + ' key: Start/Stop Recording');
+    // Completed preparation of recorder.
+    this.movRecorder = recorder;
   }
 
+  /**
+   * Movie generator function called on stopping recording.
+   * We support 2 types of container, webm and mp4.
+   */
   generateMovie() {
-    const blob = new Blob(this.chunks, { type: 'video/webm' });
+    switch (this.movType) {
+      case this.movTypeId.webm:
+        this.generateWebm();
+        break;
+      case this.movTypeId.mp4:
+        this.generateMp4();
+        break;
+    }
+  }
 
-    const url = URL.createObjectURL(blob);
+  /**
+   * Download file created with specified url and name.
+   */
+  downloadFileFromURL(url, fileName) {
     const a = document.createElement('a');
     document.body.appendChild(a);
-    a.download = this.getNowYMDhmsStr() + '.webm';
+    a.download = fileName;
     a.href = url;
     a.click();
     a.remove();
     URL.revokeObjectURL(url);
   }
 
+  /**
+   * Generate Webm movie file
+   */
+  generateWebm() {
+    const blob = new Blob(this.chunks, { type: 'video/webm' });
+    const url = URL.createObjectURL(blob);
+    const fileName = this.getNowYMDhmsStr() + '.webm';
+    this.downloadFileFromURL(url, fileName);
+  }
+
+  /**
+   * Generate mp4 movie file
+   */
+  async generateMp4() {
+    const { createFFmpeg, fetchFile } = FFmpeg;
+    const ffmpeg = createFFmpeg({
+      /* log: true */
+    });
+
+    const name = 'record.webm';
+
+    // Load ffmpeg
+    console.log('Loading ffmpeg js');
+    await ffmpeg.load();
+
+    // Start trans coding. Actually, the transcoding performance is poor now
+    // so that we only do copy(just change the container).
+    console.log('Start transcoding');
+    const blob = new Blob(this.chunks, { type: 'video/webm' });
+    ffmpeg.FS('writeFile', name, await fetchFile(blob));
+    await ffmpeg.run('-i', name, '-c', 'copy', 'output.mp4');
+
+    // Complete transcoding
+    console.log('Complete transcoding');
+    const data = ffmpeg.FS('readFile', 'output.mp4');
+
+    // File donwload procedure
+    const url = URL.createObjectURL(new Blob([data.buffer], { type: 'video/mp4' }));
+    const fileName = this.getNowYMDhmsStr() + '.mp4';
+    this.downloadFileFromURL(url, fileName);
+  }
+
+  /**
+   * Generate Webm movie file
+   */
   keyHandler() {
-    if (keyCode !== this.opeKey) {
-      // Ignore
-      return;
+    // movie type default value is 'webm'
+    this.movType = this.movTypeId.webm;
+
+    switch (keyCode) {
+      case 82: //r: start/stop Record
+        this.toggleRec();
+        break;
+      case 83: //s: start(for debug)
+        this.startRec();
+        break;
+      case 87: //w: webm
+        this.stopRec();
+        break;
+      case 77: //m: mp4
+        this.movType = this.movTypeId.mp4;
+        this.stopRec();
+        break;
+      default:
+        break;
     }
-    this.toggleRec();
   }
 
+  /**
+   * toggle Start/Stop recording
+   */
   toggleRec() {
-    if (this.movRecorder) {
-      if (this.movRecorder.state === 'inactive') {
-        console.log('🎥Start Recording.');
-        this.movRecorder.start();
-      } else if (this.movRecorder.state === 'recording') {
-        console.log('✅Recorded.');
-        this.movRecorder.stop();
-      }
+    if (!this.startRec()) {
+      this.stopRec();
     }
   }
 
-  // https://hatolabo.com/programming/js-get-now-string
+  /**
+   * Start recording
+   */
+  startRec() {
+    // Initialize chunk
+    this.chunks.length = 0;
+
+    let isStarted = false;
+    const recorder = this.movRecorder;
+    if (recorder && recorder.state === 'inactive') {
+      console.log('🎥Start Recording.');
+      recorder.start();
+      isStarted = true;
+    }
+    return isStarted;
+  }
+
+  /**
+   * Stop recording
+   */
+  stopRec() {
+    let isStopped = false;
+
+    const recorder = this.movRecorder;
+    if (recorder && recorder.state === 'recording') {
+      console.log('✅Recorded.');
+      recorder.stop();
+      isStopped = true;
+    }
+    return isStopped;
+  }
+
+  /**
+   * get current time as YYYYMMDDhhmmss format.
+   * https://hatolabo.com/programming/js-get-now-string
+   */
   getNowYMDhmsStr() {
     const date = new Date();
     const Y = date.getFullYear();
@@ -83,7 +211,7 @@ class P5MovRec {
 let p5MovRec;
 
 const initFunc = () => {
-  // Search canvas first
+  // Search 'canvas' first
   const targetCanvas = document.querySelector('canvas');
   if (targetCanvas === null) {
     // Canvas is not created yet. Call initFunc again after 100msec
